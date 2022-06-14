d5ae102e8c3be78a0f837a4336c899a65fd7cdf7
ZZDDD <ZZDDD2001@gmail.com>
2022-06-14 22:20:27 +0800

    Update Parameters

--- Parameters.m
+++ Parameters.m
@@ -4,3 +4,19 @@ theta2L = deg2rad(-40);
 theta2R = deg2rad(85);
 theta3L = deg2rad(-20);
 theta3R = deg2rad(120);
+
+% absolute position(height, offset)
+dG1 = 235.5;
+
+a1I = 260;
+d1I = 742.5-dG1;
+
+alphaI2 = deg2rad(-90);
+thetaI2 = deg2rad(-90);
+
+a23 = 945;
+theta23 = deg2rad(90);
+
+alpha36 = deg2rad(-90);
+a36 = 1025 + 220; 
+d36 = 251.5;
