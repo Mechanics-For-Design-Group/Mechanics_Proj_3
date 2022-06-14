45e4064a625beaf951374eb3aa69b076cb87cae2
ZZDDD <ZZDDD2001@gmail.com>
2022-06-14 23:06:44 +0800

    Update Parameters

--- Parameters.m
+++ Parameters.m
@@ -7,16 +7,22 @@ theta3R = deg2rad(120);
 
 % Initial D-H Parameters for alpha, a, d, theta.S
 dG1 = 235.5;
+syms theta1;
 
 a1I = 260;
 d1I = 742.5-dG1;
 
 alphaI2 = deg2rad(-90);
 thetaI2 = deg2rad(-90);
+syms theta2;
 
 a23 = 945;
 theta23 = deg2rad(90);
+syms theta3;
 
-alpha36 = deg2rad(-90);
-a36 = 1025 + 220; 
-d36 = 251.5;
+a34 = 1025;
+syms theta4;
+
+alpha46 = deg2rad(-90);
+a46 = 220; 
+d46 = 251.5;
