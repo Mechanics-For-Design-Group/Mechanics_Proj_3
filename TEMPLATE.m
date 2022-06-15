dba57d4b478c402c704d1b96c7152df88c9cdc88
ZZDDD <ZZDDD2001@gmail.com>
2022-06-15 14:27:17 +0800

    Update TEMPLATE

--- TEMPLATE.m
+++ TEMPLATE.m
@@ -14,9 +14,12 @@ alpha46 = para.alpha46;
 a46 = para.a46; 
 d46 = para.d46;
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % establish T from alpha, a, d, theta
 syms alpha a d theta
 T = [cos(theta), -sin(theta), 0, a;
     sin(theta)*cos(alpha), cos(theta)*cos(alpha), -sin(alpha), -sin(alpha)*d;
     sin(theta)*sin(alpha), cos(theta)*sin(alpha), cos(alpha), cos(alpha)*d;
     0, 0, 0, 1];
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
