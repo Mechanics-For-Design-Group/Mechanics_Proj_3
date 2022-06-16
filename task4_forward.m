1815b833ff9e349cf3455c6e2f9d72c3ba71a757
Yunpeng Ba <12010803@mail.sustech.edu.cn>
2022-06-16 19:42:23 +0800

    Update task4_forward.m

--- task4_forward.m
+++ task4_forward.m
@@ -1,8 +1,8 @@
 syms I m PC PL R theta t nl fl
 
 N=I*diff(theta,t,2)+cross(diff(theta,t,1),I*diff(theta,t,1));
-vC=cross(diff(theta,t,2),PC)+diff(theta,t,1);
-F=m*vC;
+vCdot=cross(diff(theta,t,2),PC)+diff(theta,t,1);
+F=m*vCdot;
 %f:former   l:latter   C:质心
 ff=R*fl+F;
 nf=N+R*nl+cross(PC,F)+cross(PL,R*fl);
